fa2dbcfcc6827e928fd8ea33d263774e33f6f66b
Reno Stephens <reno8946@yahoo.com>
2020-09-20 15:00:38 -0500

    Adding

--- PyPoll.py
+++ PyPoll.py
@@ -78,9 +78,9 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 
 
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
+# # Using the with statement open the file as a text file.
+# with open(file_to_save, "w") as txt_file:
 
-       # Write three counties to the file.
-     txt_file.write("Arapahoe\nDenver\nJefferson")
+#        # Write three counties to the file.
+#      txt_file.write("Arapahoe\nDenver\nJefferson")
 
